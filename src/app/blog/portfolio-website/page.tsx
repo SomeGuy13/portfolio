d39454c6c909f6606b712d45f280a73bea668a1e
SomeGuy13 <isaac7erickson@gmail.com>
2024-09-01 14:06:01 -0600

    blog 1

--- src/app/blog/portfolio-website/page.tsx
+++ src/app/blog/portfolio-website/page.tsx
@@ -2,6 +2,23 @@ export default function Page() {
   return (
     <div>
       <h1>Hello Everyone!</h1>
+      <h3>September 1, 2024</h3>
+      <br />
+      <p>
+        This is my first major project that I've managed to finish. This is my personal website were
+        I'm planning to show off all the stuff I'm doing and hopefully that gets me more motivated to finish projects...
+      </p>
+      <br />
+      <p>
+        This website is self hosted at home on a RasberryPi using NGINX for the server and JextJS
+        as a React framework. This project has been so much fun to work on, and I will continue to add
+        stuff over the years as my programming skills continue to grow and expand!
+      </p>
+      <br />
+      <p>
+        Special thanks to all those who have supported and helped me with this project. I really
+        appreciate you guys. &#60;3
+      </p>
     </div>
   )
 }
