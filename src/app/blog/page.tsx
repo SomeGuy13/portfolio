66f53af34c274d88da444ff398f77c0a629d3abb
SomeGuy13 <isaac7erickson@gmail.com>
2024-11-11 21:34:25 -0700

    rust experiment

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,7 +5,16 @@ export default function Page() {
     <div>
       <h1>Blog</h1>
       <br />
-      <BlogItem itemTitle={"Portfolio Website"} start={'I\'ve been working on a portfolio website for the past month...'} pathName={'portfolio-website'}></BlogItem>
+      <BlogItem 
+        itemTitle={"Rust Experiment"} 
+        start={'They say the best way to master something is to each someone else.'} 
+        pathName={'rs-experiment'}>
+      </BlogItem>
+      <BlogItem 
+        itemTitle={"Portfolio Website"} 
+        start={'I\'ve been working on a portfolio website for the past month...'} 
+        pathName={'portfolio-website'}>
+      </BlogItem>
     </div>
   )
 }
---
+++ src/app/blog/rs-experiment/page.tsx
@@ -0,0 +1,28 @@
+import BlogItem from '@/components/blogItem';
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Rust Experiment</h1>
+      <h3>November 11th, 2024</h3>
+      <br/>
+      <p>
+        Well, I think its time I sit down and stick with something. 
+        I&apos;ve enjoyed programming in Rust and I want to get better at it 
+        so I&apos;m going to try and teach Rust as I learn it as they say the 
+        best way to master something is to teach someone else how to do 
+        it, which is what I&apos;m going to attempt to do in the form of
+        taking notes and trying to break down what I learn further.
+      </p>
+      <br/>
+      <p>Will this work? I don&apos;t know. But it doesn&apos;t hurt to give it a try!</p>
+      <br/>
+      <h2>Part I: Rust book and Rustaceans</h2>
+      <BlogItem 
+        itemTitle={'Lesson-01: Variables'}
+        start={'Comming soon...'}
+        pathName={''}
+      ></BlogItem>
+    </div>
+  )
+}
